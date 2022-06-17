93f2c14113e75bdd95657171b5e19d2e1aae4e7d
Fabio Borges <fabio_borges10@yahoo.com.br>
2022-06-17 18:01:31 -0300

    js2

--- treino/Javascript1/index.js
+++ treino/Javascript1/index.js
@@ -4,11 +4,17 @@ const caixaDeTexto = document.querySelector('#caixaDeTexto');
 
 const botaoAdicionar = document.querySelector('#botaoAdicionar');
 
+const listaSuspensa = document.querySelector('#listaSuspensa')
+
+
+//Quando o botão adicionar for clicado 
+
 botaoAdicionar.addEventListener('click', function(){
     const textoDaTarefa = caixaDeTexto.value;
     caixaDeTexto.value = '';
 
     listaDeTarefas.appendChild(adicionarTarefas(textoDaTarefa));
+    mostrarOcultarLista();
     caixaDeTexto.focus();
 })
 
@@ -18,9 +24,22 @@ function adicionarTarefas(textoDaTarefa){
 
     elementoSpan.setAttribute('id','tarefa');
     elementoSpan.textContent = textoDaTarefa;
-
+    
+    elementoLi.className = 'naoRealizada'
     elementoLi.appendChild (elementoSpan);
     elementoLi.appendChild (botaoRemover())
+
+    //Quando uma elemento for clicado pelo mouse altera o texto
+
+    elementoSpan.addEventListener('click', function(){
+        if (this.id === 'tarefa'){
+            if(this.parentNode.className==='naoRealizada'){
+                this.parentNode.className = 'realizada'
+            } else {
+                this.parentNode.className = 'naoRealizada'
+            }
+        }
+    })
 
     return elementoLi
 }
@@ -30,9 +49,20 @@ function botaoRemover (){
     botaoTirar.textContent = '☓'
     botaoTirar.className = 'remover'
 
+    //Quando o botão remover for clicado
+
     botaoTirar.addEventListener('click', function(){
         listaDeTarefas.removeChild(this.parentNode);
     })
 
     return botaoTirar
+}
+
+function mostrarOcultarLista(){
+    const elementoSpan = document.querySelector('tarefa');
+    if(elementoSpan===null){
+        listaSuspensa.setAttribute('hidden', 'hidden')
+    }else{
+        listaSuspensa.removeAttribute('hidden')
+    }
 }
